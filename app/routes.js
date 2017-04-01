4366a22a904d3dc9eb45e7eb77707830c4eb8ddf
John Wu <john.wu@mail.mcgill.ca>
2017-03-31 20:15:45 -0400

    sdf

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,6 @@
 
 module.exports = function (app) {
 
-    // Jobs api ----------------------------------------------------------------
-
-    // get all jobs
     app.get('/api/hello', function (req, res) {
         res.json("Hello Nable");
     });
