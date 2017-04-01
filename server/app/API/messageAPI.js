3bcfc9caffff4516b47f10d9af1a31b6bc304063
Nabil <nabillionaire@live.com>
2017-04-01 11:59:41 -0400

    Text messaging works

--- server/app/API/messageAPI.js
+++ server/app/API/messageAPI.js
@@ -4,17 +4,17 @@
 // Twilio Credentials
 
 module.exports = function(to, msg){
-    var accountSid = 'AC7c27edfa4ae262264b916048268384cf';
-    var authToken = '7da1eeb44eccb5fc6b9339b39302a27a';
+    var accountSid = 'AC4cc12537e685d9d199ea0d4f06aa3aec';
+    var authToken = '89ff65673b2b8b92df329c36356c4ee0';
 
     //require the Twilio module and create a REST client
     var client = require('twilio')(accountSid, authToken);
     console.log("works");
     client.messages.create({
         to: to,
-        from: "+15876007897",
+        from: "+12264555639",
         body: msg,
     }, function(err, message) {
-        console.log();
+        //console.log(message.sid);
     });
 }
