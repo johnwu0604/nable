f34ce1dc2dab86afd8978cd96faef6fa7ed625cf
John Wu <john.wu@mail.mcgill.ca>
2017-04-02 02:03:15 -0400

    Done adding payments

--- app/Nable/app/components/InputPayment.js
+++ app/Nable/app/components/InputPayment.js
@@ -1,3 +1,56 @@
 /**
- * Created by Nabillionaire on 4/1/17.
+ * Created by TiffanyWang on 2017-04-01.
  */
+/**
+ * Created by JohnWu on 2017-04-01.
+ */
+import React, { Component } from 'react';
+import {
+    View,
+    TextInput,
+    StyleSheet
+} from 'react-native';
+
+import Dimensions from 'Dimensions';
+
+var windowHeight = Dimensions.get('window').height;
+var windowWidth = Dimensions.get('window').width;
+
+class InputPayment extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            label: this.props.text
+        };
+    }
+
+    render() {
+        return (
+            <View style={styles.box} >
+                <TextInput
+                    autoCapitalize="none"
+                    placeholder={this.state.label}
+                    style={{height: 40, borderColor: 'gray', borderWidth: 0}}
+                    onChangeText={(text) => this.props.updatePrice({text})}
+                />
+            </View>
+        );
+    }
+}
+
+
+const styles = StyleSheet.create({
+    box: {
+        backgroundColor: 'rgba(255, 255, 255, 0.3)',
+        borderColor: '#063e77',
+        borderWidth: 1,
+        borderRadius: 10,
+        height: windowHeight/12,
+        marginTop: 10,
+        marginLeft: windowWidth/12,
+        marginRight: windowWidth/12,
+    }
+});
+
+export default InputPayment;
